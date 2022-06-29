e3585e6cd5a80dcfe09dbae7271354c2c4d92d97
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-06-29 18:08:12 +0100

    added index properties

--- src/app.ts
+++ src/app.ts
@@ -111,3 +111,17 @@ const userInputElement = <HTMLInputElement>document.getElementById('user-input')
 // const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
 userInputElement.value = 'Hi there!'
 
+//index types properties
+interface ErrorContainer {
+    // {email: 'Not a valid email', username: 'Must start with a character..'}
+    // id: string;
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email',
+    username: 'Must start with a capital character!'
+};
+
+
+
