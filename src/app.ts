d3a69360afb4e9f325f04ddccc4802f056053fd6
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-06-29 13:13:11 +0100

    add getter and setter methods

--- src/app.ts
+++ src/app.ts
@@ -71,8 +71,26 @@ constructor(id: string,  admins: string[]) {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    //getter method
+    get mostRecentReport() {
+        if(this.lastReport){
+            return this.lastReport
+        }
+        throw new Error('No report found');
+    }
+
+    //setter 
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value)
+    }
     constructor(id: string,  private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
     }
 
     addEmployee(name: string) {
@@ -84,6 +102,7 @@ class AccountingDepartment extends Department {
 
     addReport(text: string) {
         this.reports.push(text)
+        this.lastReport = text;
     }
 
     printReports() {
@@ -106,7 +125,10 @@ console.log(it)
 
 const accounting = new AccountingDepartment('d2', []);
 
-accounting.addReport('Something went wrong..')
+accounting.mostRecentReport = 'Year End report';
+accounting.addReport('Something went wrong..');
+
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee('Adriana');
 accounting.addEmployee('Bobby')
