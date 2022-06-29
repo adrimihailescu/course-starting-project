b27c9783eb92dbe389ca56212018c9b7b268db12
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-06-29 16:29:23 +0100

    add interface as function example

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+// type AddFn = (a: number, b: number) => number;
+interface  AddFn {
+    (a: number, b: number): number;
+}
+
+let addNum: AddFn;
+
+addNum = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
+
+
 interface Named {
     readonly name: string;
 }
