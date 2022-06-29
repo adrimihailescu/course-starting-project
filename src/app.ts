bee6a34591acc3df9fcdf0e6b4f5407dc05feb6f
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-06-29 12:07:28 +0100

    add first class with constructor and this keyword

--- src/app.ts
+++ src/app.ts
@@ -14,25 +14,46 @@
 
 // age = 35;
     
-function addNumbers(a: number, b: number) {
-    let result;
-    result = a + b;
-    return result
+// function addNumbers(a: number, b: number) {
+//     let result;
+//     result = a + b;
+//     return result
+// }
+// // console.log(result);
+
+
+// //destructuring
+// const hobbies = ['Hiking', 'Dancing']
+
+// const [hobbie1, hobbie2, ...remainingHobbies] = hobbies;
+
+// console.log(hobbies, hobbie1, hobbie2)
+
+
+// const personNew = {
+//     firstName: 'Adriana',
+//     age: 36
+// }
+
+// const {firstName, age} = personNew;
+
+class Department {
+    name: string;
+
+    constructor(n: string){
+        this.name = n;
+    }
+
+    describe(this: Department){
+        console.log('Department: ' + this.name)
+    }
 }
-// console.log(result);
 
+const accounting = new Department('Accounting');
 
-//destructuring
-const hobbies = ['Hiking', 'Dancing']
+// console.log(accounting)
+accounting.describe()
 
-const [hobbie1, hobbie2, ...remainingHobbies] = hobbies;
+const accountingCopy = {name: 'Dummy', describe: accounting.describe};
 
-console.log(hobbies, hobbie1, hobbie2)
-
-
-const personNew = {
-    firstName: 'Adriana',
-    age: 36
-}
-
-const {firstName, age} = personNew;
+accountingCopy.describe()
