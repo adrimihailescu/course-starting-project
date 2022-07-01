3c84feed3dceb5eaab364472a2207b418d5b89c9
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-01 10:44:54 +0100

    add and manage application state with singletons

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,44 @@
+//Project State Management
+
+class ProjectState {
+  private listeners: any[]= [];
+  private projects: any[] = [];
+  private static instance: ProjectState;
+
+  private constructor() {
+
+  }
+
+  static getInstance() {
+    if(this.instance) {
+      return this.instance;
+    }
+    this.instance = new ProjectState();
+    return this.instance;
+  }
+
+  addListener(listenerFn: Function) {
+    this.listeners.push(listenerFn);
+  }
+
+  addProject(title: string, description: string, numOfPeople: number) {
+    //project we want to store
+    const newProject = {
+      id: Math.random().toString(), //generate a random id
+      title: title,
+      description: description,
+      people: numOfPeople
+    };
+    this.projects.push(newProject);
+    for (const listenerFn of this.listeners) {
+      listenerFn(this.projects.slice()) // copy of projects
+    }
+  }
+}
+
+const projectState = ProjectState.getInstance();
+
+
 // Validation
 interface Validatable {
     value: string | number;
@@ -60,12 +101,14 @@ interface Validatable {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
+    assignedProjects: any[];
 
     constructor(private type: 'active' | 'finished'){
       this.templateElement = document.getElementById(
         'project-list'
       )! as HTMLTemplateElement;
       this.hostElement = document.getElementById('app')! as HTMLDivElement;
+      this.assignedProjects = [];
   
       const importedNode = document.importNode(
         this.templateElement.content,
@@ -74,12 +117,26 @@ interface Validatable {
       this.element = importedNode.firstElementChild as HTMLElement;
       this.element.id = `${this.type}-projects`;
 
+      projectState.addListener((projects: any[]) => {
+        this.assignedProjects = projects;
+        this.renderProjects();
+      }); //register a listener fuunction
+
       this.attach();
       this.renderContent();
     }
 
+    private renderProjects(){
+      const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+      for (const prjItem of this.assignedProjects) {
+        const listItem = document.createElement('li');
+        listItem.textContent = prjItem.title;
+        listEl.appendChild(listItem);
+      }
+    }
+
     private renderContent() {
-      const listId = `${this.type}-project-list`;
+      const listId = `${this.type}-projects-list`;
       this.element.querySelector('ul')!.id = listId;
       this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
     }
@@ -171,7 +228,7 @@ interface Validatable {
       const userInput = this.gatherUserInput();
       if (Array.isArray(userInput)) {
         const [title, desc, people] = userInput;
-        console.log(title, desc, people);
+        projectState.addProject(title, desc, people);
         this.clearInputs();
       }
     }
